e83e1e1ee1d8f4806037ab378e1dfcf40d6b8a4f
Lukáš Dastych <Luk Dastych@notebook1-hp>
2013-02-22 22:48:24 +0100

    Prvni commit - vytvoření projektu semestrální práce - Rodinná databáze

---
+++ rodinna_databaze/src/rodinna_databaze/Rodinna_databaze.java
@@ -0,0 +1,15 @@
+package rodinna_databaze;
+
+/**
+ *
+ * @author Lukáš Dastych
+ */
+public class Rodinna_databaze {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
