4836db4e758ee3108e9a74d84360c45f1ad7cb74
Lukáš Dastych <Luk Dastych@notebook1-hp>
2013-03-08 14:48:47 +0100

    Zkouska prace s databazi SQL

--- rodinna_databaze/src/rodinna_databaze/Rodinna_databaze.java
+++ rodinna_databaze/src/rodinna_databaze/Rodinna_databaze.java
@@ -1,5 +1,7 @@
 package rodinna_databaze;
 
+import java.sql.*;
+
 /**
  * Semestrální práce na A0B36PR2
  * RODINNÁ DATABÁZE
@@ -11,7 +13,36 @@ public class Rodinna_databaze {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws Exception{
+        String url = "jdbc:derby://localhost:1527/Datab1";
+        Class.forName("org.apache.derby.jdbc.ClientDriver").newInstance();
+        Connection conn = DriverManager.getConnection(url, "datab1", "datab1");
+
+        Statement st = conn.createStatement();
+        
+        //st.executeUpdate("INSERT INTO \"APP\".\"ZAMESTNANCI\" (ZAMESTNANECID,PRIJMENI,JMENO,JMENO2,ZAMESTNAVATEL,DATUM_NAROZENI) VALUES (7,'Novotny','Honza','Jan.',1,{d '1994-07-08'})");        
+        //st.executeUpdate("UPDATE APP.ZAMESTNANCI SET JMENO2='Jan' WHERE ZAMESTNANECID=7");
+        //st.executeUpdate("DELETE FROM APP.ZAMESTNANCI WHERE ZAMESTNANECID=7");
+        
+        ResultSet rs = st.executeQuery(
+                "SELECT APP.ZAMESTNANCI.ZAMESTNANECID, APP.ZAMESTNANCI.JMENO, APP.ZAMESTNANCI.PRIJMENI, APP.ZAMESTNANCI.DATUM_NAROZENI, APP.FIRMY.NAZEV, APP.FIRMY.MESTO "
+                + "FROM APP.ZAMESTNANCI JOIN APP.FIRMY "
+                + "ON APP.ZAMESTNANCI.ZAMESTNAVATEL = APP.FIRMY.FIRMAID");
+
+        // vypsání výsledku
+        System.out.printf("%14s  %-10s  %-10s %-12s --  %-20s %-40s%n", "ID zamestnance", "jméno", "příjmení", "datum naroz", "zaměstnavatel", "město");
+        int pruchod = 0;
+        while (rs.next()) {
+            pruchod++;
+            if (pruchod != 3) {
+            System.out.printf("%14s  %-10s  %-10s %-12s --  %-20s %-40s%n", rs.getString(1), rs.getString(2), rs.getString(3), rs.getDate(4), rs.getString(5), rs.getString(6));
+            }
+        }
+        
+        // uzavření dotazu i všech výsledků
+        st.close();
+
+        // uzavření spojení
+        conn.close();
     }
 }
